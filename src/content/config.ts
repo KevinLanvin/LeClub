4e013d262db14afbd4a7f31b79c1e0e577c6bc00
Kiwin <klanvin@partner.auchan.fr>
2026-05-05 17:10:08 +0200

    Planning structure

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,44 @@
 import { defineCollection, z } from "astro:content";
 
+const prerequisiteValues = [
+  "2 - 4 mois",
+  "4 - 6 mois",
+  "6 mois et +",
+  "Rééducation",
+  "Intermédiaire",
+  "Avancé",
+  "CSAU",
+] as const;
+
+const teachers = [
+  "sandrine",
+  "laurent",
+  "fred",
+  "lena",
+  "nathalie",
+  "yohann",
+  "marie",
+  "patrick",
+  "anthony",
+  "valerie",
+  "christophe",
+  "badis",
+  "kevin",
+  "guillaume",
+  "dominique",
+] as const;
+
 const coursesCollection = defineCollection({
   type: "content",
   schema: z.object({
-    text: z.string(),
+    date: z.date(),
+    courses: z.array(
+      z.object({
+        hour_interval: z.string(),
+        courseName: z.enum(prerequisiteValues),
+        teachers: z.array(z.enum(teachers)),
+      }),
+    ),
   }),
 });
 
---
+++ src/content/courses/index.md
@@ -0,0 +1,34 @@
+---
+date: 2026-05-05
+courses:
+  - hour_interval: "09:00 - 10:00"
+    courseName: "2 - 4 mois"
+    teachers:
+      - sandrine
+      - laurent
+  - hour_interval: "10:00 - 11:00"
+    courseName: "4 - 6 mois"
+    teachers:
+      - fred
+      - lena
+  - hour_interval: "11:00 - 12:00"
+    courseName: "6 mois et +"
+    teachers:
+      - nathalie
+      - yohann
+  - hour_interval: "14:00 - 15:00"
+    courseName: Intermédiaire
+    teachers:
+      - marie
+      - patrick
+  - hour_interval: "15:00 - 16:00"
+    courseName: Avancé
+    teachers:
+      - anthony
+      - valerie
+  - hour_interval: "16:00 - 17:00"
+    courseName: Rééducation
+    teachers:
+      - christophe
+      - badis
+---
